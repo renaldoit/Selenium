6d9e9168400db10a14a76c39d434aa1a7f98f800
renaldoit <122207239+renaldoit@users.noreply.github.com>
2025-02-18 21:30:25 +0700

    angkot 3

--- JavaScript/angkot3(for).js
+++ JavaScript/angkot3(for).js
@@ -3,8 +3,10 @@ var angkotBeroperasi = 8;
 var noAngkot = 1;
 
 
-for(noAngkot = angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++) {
-    console.log('Angkot No. ' + noAngkot + ' sedang tidak beroperasi.');
-}else (noAngkot <= angkotBeroperasi && noAngkot !== 5) {
+for(noAngkot = angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++){
+if (noAngkot <= angkotBeroperasi) {
     console.log('Angkot No. ' + noAngkot + ' sedang beroperasi dengan baik.');
+} else (noAngkot <= angkotBeroperasi) {
+    console.log('Angkot No. ' + noAngkot + ' sedang tidak beroperasi.');
+}
 }
